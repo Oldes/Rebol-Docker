52ddb149876e9644aa10a63e6627a63040da7d93
Oldes <oldes.huhuman@gmail.com>
2021-12-22 21:17:31 +0100

    Rebol Alpine build

--- alpine/x86_64/build-all.sh
+++ alpine/x86_64/build-all.sh
@@ -3,12 +3,16 @@
 # this script is supposed to be run from rebol-dev container
 
 cd /rebol/Builder/
+/bin/rebol siskin.r3 rebol --list
 /bin/rebol siskin.r3 rebol 1
 /bin/rebol siskin.r3 rebol 2
 /bin/rebol siskin.r3 rebol 3
-mv ./tree/rebol/Rebol/build/rebol3-base-x64-libc-gcc /out/rebol3-base-x64-musl
-mv ./tree/rebol/Rebol/build/rebol3-core-x64-libc-gcc /out/rebol3-core-x64-musl
-mv ./tree/rebol/Rebol/build/rebol3-bulk-x64-libc-gcc /out/rebol3-bulk-x64-musl
-chmod a+x /out/rebol3-base-x64-musl
-chmod a+x /out/rebol3-core-x64-musl
-chmod a+x /out/rebol3-bulk-x64-musl
+mv ./tree/rebol/Rebol/build/rebol3-base-linux-x64 /out/rebol3-base-linux-x64-musl
+mv ./tree/rebol/Rebol/build/rebol3-core-linux-x64 /out/rebol3-core-linux-x64-musl
+mv ./tree/rebol/Rebol/build/rebol3-bulk-linux-x64 /out/rebol3-bulk-linux-x64-musl
+chmod a+x /out/rebol3-base-linux-x64-musl
+chmod a+x /out/rebol3-core-linux-x64-musl
+chmod a+x /out/rebol3-bulk-linux-x64-musl
+gzip -9 /out/rebol3-base-linux-x64-musl
+gzip -9 /out/rebol3-core-linux-x64-musl
+gzip -9 /out/rebol3-bulk-linux-x64-musl
